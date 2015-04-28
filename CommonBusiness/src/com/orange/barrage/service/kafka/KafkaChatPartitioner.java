fc19baf1da9b5b538ec448cae99ca0f3f7c1e025
Benson Peng <penglingzhe@gmail.com>
2015-04-28 17:08:00 +0800

    update

--- CommonBusiness/src/com/orange/barrage/service/kafka/KafkaChatPartitioner.java
+++ CommonBusiness/src/com/orange/barrage/service/kafka/KafkaChatPartitioner.java
@@ -1,5 +1,6 @@
 package com.orange.barrage.service.kafka;
 
+import com.orange.protocol.message.UserProtos;
 import kafka.producer.Partitioner;
 
 /**
@@ -9,7 +10,13 @@ public class KafkaChatPartitioner implements Partitioner {
 
     @Override
     public int partition(Object o, int numPartitions) {
-        return o.hashCode() % numPartitions;
+        if (o instanceof UserProtos.PBChat){
+            UserProtos.PBChat pbChat = (UserProtos.PBChat)o;
+            return pbChat.getFromUserId().hashCode() % numPartitions;
+        }
+        else {
+            return o.hashCode() % numPartitions;
+        }
     }
 
 }
